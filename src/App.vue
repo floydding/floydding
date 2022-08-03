f4772ea1d35a6b875d41870e8abcba51f10f04d7
Floyd Ding <floydding@gmail.com>
2022-08-03 11:03:59 -0400

    feat: add screen size breakpoint

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <router-view/>
+  <div class="wrapper">
+    <router-view/>
+  </div>
 </template>
+
+<style scoped>
+.wrapper {
+  padding: 1rem;
+}
+
+@media only screen and (min-width: 768px) {
+  .wrapper {
+    width: 50%;
+    margin: auto;
+  }
+}
+</style>
